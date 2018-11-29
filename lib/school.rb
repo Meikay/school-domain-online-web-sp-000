ff0ed2b7dd7b808d66389d781b2567a96fc3fd49
Meisuleen Kased <meisuleen@gmail.com>
2018-11-29 01:43:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def roster
+  @roster
+end
+
+def add_student(student_name, student_grade)
+  if @roster.include?(student_grade) == false
+    @roster[student_grade] = []
+  end
+    @roster[student_grade] << student_name
+end
+
+def grade(grade)
+  grade
+end
+
+
+end
